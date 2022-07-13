694fc34a33517aa13498eae4a18ea243141a7049
Fabio Sinobre <engenharia.computacao4.0@gmail.com>
2022-07-13 11:16:06 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-  return(
-    <h1>Boa noite!</h1>
+  return (
+    <>
+      <h1>Boa noite!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
