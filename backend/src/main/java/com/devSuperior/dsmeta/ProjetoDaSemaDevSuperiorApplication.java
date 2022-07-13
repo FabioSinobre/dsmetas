1633d844c13d258b3c4330a1563637bc5cd18ea0
Fabio Sinobre <engenharia.computacao4.0@gmail.com>
2022-07-12 21:49:38 -0300

    Project created

---
+++ backend/src/main/java/com/devSuperior/dsmeta/ProjetoDaSemaDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaSemaDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaSemaDevSuperiorApplication.class, args);
+	}
+
+}
